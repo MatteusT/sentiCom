cd353a8a6dc7f4dde9903c048386a99e35e65e18
MatteusT <matteus.tanha@gmail.com>
2016-02-03 11:34:46 -0500

    minor change

--- explore/views.py
+++ explore/views.py
@@ -5,5 +5,5 @@ def indexPage(request):
     if request.method == "POST":
          rval = 'Sebbs chos hast'#float(request.POST.get('numvalue'))**2
     else:
-         rval = 'Sebbs chos hast'
+         rval = ''
     return render(request, 'explore/index.html', {'rval': rval})# Create your views here.
