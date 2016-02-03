9a5bfde9f1621873de313d3ccc61985aa015572b
MatteusT <matteus.tanha@gmail.com>
2016-02-03 11:32:33 -0500

    minor change

--- explore/views.py
+++ explore/views.py
@@ -3,7 +3,7 @@ from django.shortcuts import render
 # Create your views here.
 def indexPage(request):
     if request.method == "POST":
-         rval = float(request.POST.get('numvalue'))**2
+         rval = 'Sebbs chos hast'#float(request.POST.get('numvalue'))**2
     else:
-         rval = ''
+         rval = 'Sebbs chos hast'
     return render(request, 'explore/index.html', {'rval': rval})# Create your views here.
